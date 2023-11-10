b3887484a1967be83e507ff057f7b68559adcca4
Jonathan <jvasquezqm@gmail.com>
2023-11-10 15:19:01 -0500

    Archivos sin seguimiento: src/components/Gestion/ src/components/TableTemplate.vue src/pages/ClientPage.vue src/pages/LoginForm.vue

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,12 +5,13 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'Inicio', component: () => import('components/auth/LoginForm.vue') }
+      { path: "client", component: () => import("pages/ClientPage.vue") },
+      { path: 'Inicio', component: () => import("pages/LoginForm.vue") },
     ]
   },
   {
     path: '/',
-    component: () => import('components/auth/LoginForm.vue')
+    component: () => import('pages/LoginForm.vue')
   },
   // Always leave this as last one,
   // but you can also remove it
