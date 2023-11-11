41688b3303e2c7ac93b0c4f35a0f5ae65be82738
Daniel <143299321+20112058@users.noreply.github.com>
2023-11-11 17:56:23 -0500

    FE Propietario

---
+++ src/pages/GestionPropietario.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="propietario-page">
+        <div class="titulo"><h5>GESTIÓN PROPIETARIO</h5></div>
+        <PropietarioAdd ref="PropietarioAdd" />
+        <div class="agregar"><q-btn @click="agregarv" color="blue" icon="add" label="Añadir Propietario" /></div>
+        <br/>
+        <div class="tabla"><PropietarioTabla /></div>
+    </div>  
+</template>
+
+
+
+<style>
+
+.propietario-page{
+
+    justify-content: space-between;
+}
+.titulo {
+    text-align: center;
+    width: 100%;
+}
+
+.agregar {
+    text-align: left;
+    margin-left: 2%;
+}
+.tabla {
+    text-align: center;
+    width: 100%;
+}
+</style>
+
+
+<script>
+
+import PropietarioTabla from 'src/components/Propietario/PropietarioTabla.vue';
+import PropietarioAdd from 'src/components/Propietario/PropietarioAdd.vue'
+
+export default {
+    name: "GestionPropietario",
+    components: {
+        PropietarioTabla,
+        PropietarioAdd
+    },
+
+    methods: {
+        agregarv(){
+            this.$refs.PropietarioAdd.abrirModalnuevoV();
+        }
+    }
+}
+
+</script>
