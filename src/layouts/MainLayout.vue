5689be98e4c64f1ee19eb660152ac126d189306d
Jonathan <jvasquezqm@gmail.com>
2023-11-11 19:46:10 -0500

    toolbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,28 +1,37 @@
+mainlyaout
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated >
       <q-toolbar>
-    <q-tabs
-      v-model="activeTab"
-      align="center"
-      class="bg-primary text-white"
-    >
-      <q-tab
-        v-for="(tab, index) in tabs"
-        :key="index"
-        :label="tab.label"
-        :name="tab.name"
-      />
-
-    </q-tabs>
-    <!-- Botón de Cerrar Sesión -->
-    <q-btn flat dense round
-      icon="exit_to_app"
-      @click="cerrarSesion"
-      class="q-ml-auto"
-
-    />
-  </q-toolbar>
+        <q-tabs
+          v-model="activeTab"
+          align="center"
+          class="bg-primary text-white"
+        >
+          <q-tab
+            v-for="(tab, index) in tabs"
+            :key="index"
+            :label="tab.label"
+            :name="tab.name"
+            @click="cambiarRuta(tab.name)"
+          />
+          <q-menu v-if="activeTab === 'cliente'">
+            <q-list link>
+              <q-item @click="cambiarRuta('cliente')">cliente</q-item>
+              <q-item @click="cambiarRuta('vehiculo')">Vehículos</q-item>
+            </q-list>
+          </q-menu>
+        </q-tabs>
+        <!-- Botón de Cerrar Sesión -->
+        <q-btn
+          flat
+          dense
+          round
+          icon="exit_to_app"
+          @click="cerrarSesion"
+          class="q-ml-auto"
+        />
+      </q-toolbar>
     </q-header>
 
 
@@ -33,8 +42,8 @@
 </template>
 
 <script>
+import { useRoute } from 'vue-router';
 import { defineComponent, ref } from 'vue'
-
 
 
 export default defineComponent({
@@ -47,20 +56,63 @@ export default defineComponent({
     cerrarSesion() {
       // Lógica para cerrar sesión
       console.log('Cerrar Sesión');
-      // Puedes redirigir a la página de inicio de sesión, por ejemplo:
-      // this.$router.push('/login');
+      this.$router.push('/');
+
+    },
+    cambiarRuta(name) {
+      this.$router.push(`/dashboard/${name}`);
+
+      if (name === 'client' || name === 'vehiculo') {
+
+      } else {
+        switch (name) {
+          case 'home':
+            this.$router.push('/dashboard');
+            break;
+          case 'gestionTaller':
+            this.$router.push('/gestion-taller');
+            break;
+          case 'propietario':
+            this.$router.push('/dashboard/propietario');
+            break;
+          case 'cliente':
+            this.$router.push('/dashboard/client'); // Cambia la ruta a "/cliente"
+            break;
+          case 'vehiculo':
+            this.$router.push('/dashboard/vehiculo'); // Cambia la ruta a "/vehiculo"
+            break;
+          case 'servicios':
+            this.$router.push('/dashboard/servicios');
+            break;
+          case 'facturas':
+            this.$router.push('/dashboard/facturas');
+            break;
+          case 'inventario':
+            this.$router.push('/dashboard/inventario');
+            break;
+          case 'reportes':
+            this.$router.push('/dashboard/reportes');
+            break;
+          default:
+            break;
+        }
+      }
     },
   },
 
   setup () {
+    var route = useRoute();
     var leftDrawerOpen = ref(false)
 
     return {
-      activeTab: 'perfil', // Nombre de la pestaña activa inicial
+      activeTab: 'home', // Nombre de la pestaña activa inicial
       tabs: [
-        { name: 'perfil', label: 'Perfil' },
+        { name: 'home', label: 'Home' },
         { name: 'gestionTaller', label: 'Gestión Taller' },
-        { name: 'clientes', label: 'Gestion Clientes' },
+        { name: 'propietario', label: 'Gestión Propietarios' },
+        { name: 'cliente', label: 'Gestion cliente' },
+        { name: 'vehiculo', label: 'Vehiculo' },
+
         { name: 'servicios', label: 'Servicios' },
         { name: 'facturas', label: 'Facturas' },
         { name: 'inventario', label: 'Inventario' },
--- src/pages/ClientPage.vue
+++ src/pages/ClientPage.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="product-page">
+    <div class="titulo"><h5>GESTIÓN CLIENTE</h5></div>
+    <ClientAdd ref="ClientAdd" />
     <div class="q-ml-md q-mr-xl">
-      <ClientGestion />
+      <div class="agregar"><q-btn @click="agregarc" @clienteAgregado="actualizarTabla" color="blue" icon="add" label="Añadir Cliente" /></div>
+        <br/>
+      <div class="tabla"><ClientGestion /></div>
     </div>
   </div>
 </template>
 <style>
 .product-page {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+}
+
+.titulo {
+text-align: center;
+width: 100%;
+}
+
+.agregar {
+text-align: left;
+margin-left: 2%;
+}
+.tabla {
+text-align: center;
+width: 100%;
 }
 
 
@@ -19,12 +34,24 @@
 </style>
 <script>
 import ClientGestion from "src/components/Cliente/ClientGestion.vue";
+import ClientAdd from 'src/components/Cliente/ClientAdd.vue'
 export default {
   name: "ClientPage",
   components: {
     ClientGestion,
+    ClientAdd,
 
+  },
+  methods: {
+    async agregarc(){
+      this.$refs.ClientAdd.abrirModalnuevoC();
+      await this.getDatos();
+    },
+    async actualizarTabla() {
+      await this.getDatos();
+    },
   }
 };
+
 
 </script>
